d382a4e78cc8726e141c408a40923dcdf52a4ac1
Sharathjayan01 <sharathjayanpondy@gmail.com>
2024-09-10 12:14:19 +0530

    sharathjayan

--- ngo_admin/lib/HomePage.dart
+++ ngo_admin/lib/HomePage.dart
@@ -12,54 +12,33 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
-  int _currentIndex=0;
-  List<Widget> screens=[Home(),VolunteerRequestHandler()];
-
+  int _currentIndex = 0;
+  List<Widget> screens = [Home(), VolunteerRequestHandler()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (index){
-          _currentIndex=index;
-          setState(() {
-            _currentIndex;
-
-          });
-
-        },
+          onTap: (index) {
+            _currentIndex = index;
+            setState(() {
+              _currentIndex;
+            });
+          },
           currentIndex: _currentIndex,
           selectedItemColor: Colors.blue,
-
-
-
-
-
           items: [
-        
-        
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: "HOME"),
-        BottomNavigationBarItem(icon: Icon(Icons.hail),label: "VOLUNTEER REQUEST")
-        
-      ]),
-
-
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "HOME"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.hail), label: "VOLUNTEER REQUEST")
+          ]),
       appBar: AppBar(
         title: Text("HomePage"),
-        
       ),
-
-
       body: screens[_currentIndex],
-      
-      
     );
   }
-
 }
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -71,84 +50,90 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(stream: FirebaseFirestore.instance.collection("query").snapshots(), builder: (context,snapshot){
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("query").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return SizedBox();
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  print("Clicked card ${snapshot.data?.docs.length}");
 
-      if(snapshot.data==null)
-        {
-          return SizedBox();
-        }
-      return ListView.builder(itemBuilder: (context,index){
-
-        return InkWell(
-          onTap: (){
-            print("Clicked card ${snapshot.data?.docs.length}");
-
-            Navigator.push(context, MaterialPageRoute(builder: (ctx)=>DonatedPeopleList(options:snapshot.data?.docs[index]['options'].toString(),)));
-          },
-          child: Card(
-            child: Container(
-              margin: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-
-                children: [
-                  Row(
-
-                    children: [
-                      CircleAvatar(
-
-                        child: Text("${snapshot.data?.docs[index]['name'][0].toString().toUpperCase()}"),
-                      ),
-
-                      SizedBox(width: 20,),
-                      Text("${snapshot.data?.docs[index]['name'].toString()}",style: GoogleFonts.aBeeZee(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      ),),
-
-                    ],
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (ctx) => DonatedPeopleList(
+                                options: snapshot.data?.docs[index]['options']
+                                    .toString(),
+                                queryId:
+                                    snapshot.data?.docs[index].id.toString(),
+                              )));
+                },
+                child: Card(
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: Text(
+                                  "${snapshot.data?.docs[index]['name'][0].toString().toUpperCase()}"),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "${snapshot.data?.docs[index]['name'].toString()}",
+                              style: GoogleFonts.aBeeZee(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 60),
+                          child: Text(
+                            "${snapshot.data?.docs[index]['phone'].toString()}",
+                            style: GoogleFonts.aBeeZee(
+                                color: Colors.grey, fontSize: 18),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 60),
+                          child: Text(
+                            "${snapshot.data?.docs[index]['queryname'].toString()}",
+                            style: GoogleFonts.aBeeZee(
+                                color: Colors.red, fontSize: 18),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 60),
+                          child: Text(
+                            "${snapshot.data?.docs[index]['options'].toString()}",
+                            style: GoogleFonts.aBeeZee(
+                                color: Colors.blue, fontSize: 18),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 60),
+                          child: Text(
+                            "${snapshot.data?.docs[index]['discription'].toString()}",
+                            style: GoogleFonts.aBeeZee(
+                                color: Colors.purple, fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-
-                  Container(
-                    padding: EdgeInsets.only(left: 60),
-                    child: Text("${snapshot.data?.docs[index]['phone'].toString()}",style: GoogleFonts.aBeeZee(
-                        color: Colors.grey,
-                        fontSize: 18
-                    ),),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.only(left: 60),
-                    child: Text("${snapshot.data?.docs[index]['queryname'].toString()}",style: GoogleFonts.aBeeZee(
-                        color: Colors.red,
-                        fontSize: 18
-                    ),),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.only(left: 60),
-                    child: Text("${snapshot.data?.docs[index]['options'].toString()}",style: GoogleFonts.aBeeZee(
-                        color: Colors.blue,
-                        fontSize: 18
-                    ),),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 60),
-                    child: Text("${snapshot.data?.docs[index]['discription'].toString()}",style: GoogleFonts.aBeeZee(
-                        color: Colors.purple,
-                        fontSize: 18
-                    ),),
-                  ),
-
-
-                ],
-              ),
-            ),
-          ),
-        );
-      },itemCount: snapshot.data?.docs.length,);
-
-    });
+                ),
+              );
+            },
+            itemCount: snapshot.data?.docs.length,
+          );
+        });
   }
 }
-
--- ngo_admin/lib/donatedPeopleMapping.dart
+++ ngo_admin/lib/donatedPeopleMapping.dart
@@ -1,13 +1,13 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ngo_admin/volunteer_assignment.dart';
 
 class DonatedPeopleList extends StatefulWidget {
   final String? options;
-  const DonatedPeopleList({super.key,required this.options});
+  final String? queryId;
+  const DonatedPeopleList({super.key, required this.options, this.queryId});
 
   @override
   State<DonatedPeopleList> createState() => _DonatedPeopleListState();
@@ -16,73 +16,96 @@ class DonatedPeopleList extends StatefulWidget {
 class _DonatedPeopleListState extends State<DonatedPeopleList> {
   @override
   Widget build(BuildContext context) {
-    var height=MediaQuery.of(context).size.height;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-
-
-
       appBar: AppBar(
         title: Text("List of people Donated"),
       ),
-      
-
-      body: StreamBuilder(stream: FirebaseFirestore.instance.collection("requirements").where('options',isEqualTo: widget.options.toString()).snapshots(), builder: (context,snapshots)
-      {
-        
-        if(snapshots.connectionState==ConnectionState.active)
-          {
-            return  ListView.builder(itemBuilder: (context,index){
-
-              var place="";
-               getCurrentPlace(latitude: double.parse("${snapshots.data?.docs[index]['latitude']}"),longitude:double.parse("${snapshots.data?.docs[index]['longitude']}"));
-
-
-
-
-              return Card(
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                  
-                   
-                      Text("${snapshots.data?.docs[index]['pname'].toString()}",style: GoogleFonts.aBeeZee(color: Colors.red,fontSize: 20),),
-                     SizedBox(height: 10,),
-                      Text("${snapshots.data?.docs[index]['pdes'].toString()}",style: GoogleFonts.aBeeZee(color: Colors.blue,fontSize: 20),)
-                     ,SizedBox(height: 20,),
-                     Text("Sample image will be",style: GoogleFonts.aBeeZee(fontSize: 15),),
-                     SizedBox(height: 10,),
-
-                     RichText(text: TextSpan(
-text: "${place}",
-                       style: GoogleFonts.aBeeZee(color: Colors.blue)
-                     )),
-                     Image.network("${snapshots.data?.docs[index]['imageUrl'].toString()}",width: double.infinity,height:height/2 ,fit: BoxFit.fill,)
-                    ],
-                  ),
-                ),
-              );
-            },itemCount: snapshots.data?.docs.length,);
-          }
-        if(snapshots.hasError)
-          {
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("requirements")
+              .where('options', isEqualTo: widget.options.toString())
+              .snapshots(),
+          builder: (context, snapshots) {
+            if (snapshots.connectionState == ConnectionState.active) {
+              return ListView.builder(
+                  itemCount: snapshots.data?.docs.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    AssignVolunteerToQueryAndRequirement(
+                                        queryId: widget.queryId,
+                                        requirementId: snapshots
+                                            .data?.docs[index].id
+                                            .toString(),
+                                        lat: double.parse(snapshots
+                                            .data?.docs[index]['latitude']),
+                                        long: double.parse(snapshots
+                                            .data?.docs[index]['longitude']))));
+                      },
+                      child: Card(
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Text(
+                                "${snapshots.data?.docs[index]['pname'].toString()}",
+                                style: GoogleFonts.aBeeZee(
+                                    color: Colors.red, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "${snapshots.data?.docs[index]['pdes'].toString()}",
+                                style: GoogleFonts.aBeeZee(
+                                    color: Colors.blue, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "Sample image will be",
+                                style: GoogleFonts.aBeeZee(fontSize: 15),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                  text: TextSpan(
+                                      text: "hell",
+                                      style: GoogleFonts.aBeeZee(
+                                          color: Colors.blue))),
+                              Image.network(
+                                "${snapshots.data?.docs[index]['imageUrl'].toString()}",
+                                width: double.infinity,
+                                height: height / 2,
+                                fit: BoxFit.fill,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            }
+            if (snapshots.hasError) {
+              return SizedBox();
+            }
             return SizedBox();
-          }
-        return SizedBox();
-      }
-
-
-
-
-      ),
+          }),
     );
   }
 
-
-  Future<String> getCurrentPlace({required double latitude,required double longitude}) async
-  {
-     List<Placemark> placeMarks=await placemarkFromCoordinates(latitude, longitude);
-     return "${placeMarks[0].street} ${placeMarks[0].name}";
+  Future<String> getCurrentPlace(
+      {required double latitude, required double longitude}) async {
+    List<Placemark> placeMarks =
+        await placemarkFromCoordinates(latitude, longitude);
+    return "${placeMarks[0].street} ${placeMarks[0].name}";
   }
 }
